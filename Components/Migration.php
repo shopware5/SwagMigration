28c3bac8f7bd26eb69d151a4634831e58fd4adab
Daniel Nögel <dn@shopware.de>
2012-12-04 09:17:27 +0000

    PLUG-291 - Adds components headers

--- Components/Migration.php
+++ Components/Migration.php
@@ -1,15 +1,33 @@
 <?php
+/**
+ * Shopware 4.0
+ * Copyright © 2012 shopware AG
+ *
+ * According to our dual licensing model, this program can be used either
+ * under the terms of the GNU Affero General Public License, version 3,
+ * or under a proprietary license.
+ *
+ * The texts of the GNU Affero General Public License with an additional
+ * permission and of our proprietary license can be found at and
+ * in the LICENSE file you have received along with this program.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ *
+ * "Shopware" is a registered trademark of shopware AG.
+ * The licensing of the program under the AGPLv3 does not imply a
+ * trademark license. Therefore any rights, title and interest in
+ * our trademarks remain entirely with us.
+ */
 
 /**
- * Shopware Migration Component is an extension of the enlight class.
- * This class has an factory to create the profile object for runtime
+ * Shopware SwagMigration Components - Migration
  *
- * @copyright Copyright (c) 2011, Shopware AG
- * @author o.denter
- * @package Shopware
- * @subpackage Controllers_Frontend
- * @copyright Copyright (c) 2011, shopware AG
- * @version
+ * @category  Shopware
+ * @package Shopware\Plugins\SwagMigration\Components
+ * @copyright Copyright (c) 2012, shopware AG (http://www.shopware.de)
  */
 class Shopware_Components_Migration extends Enlight_Class
 {
--- Components/Migration/Profile/Prestashop.php
+++ Components/Migration/Profile/Prestashop.php
@@ -1,13 +1,33 @@
 <?php
 /**
- * PrestaShop profile class to load all shop system data from the PrestaShop database.
+ * Shopware 4.0
+ * Copyright © 2012 shopware AG
  *
- * @copyright Copyright (c) 2011, Shopware AG
- * @author d.noegel
- * @package Shopware
- * @subpackage Components_Migration
- * @copyright Copyright (c) 2012, shopware AG
- * @version
+ * According to our dual licensing model, this program can be used either
+ * under the terms of the GNU Affero General Public License, version 3,
+ * or under a proprietary license.
+ *
+ * The texts of the GNU Affero General Public License with an additional
+ * permission and of our proprietary license can be found at and
+ * in the LICENSE file you have received along with this program.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ *
+ * "Shopware" is a registered trademark of shopware AG.
+ * The licensing of the program under the AGPLv3 does not imply a
+ * trademark license. Therefore any rights, title and interest in
+ * our trademarks remain entirely with us.
+ */
+
+/**
+ * Shopware SwagMigration Components - Prestashop
+ *
+ * @category  Shopware
+ * @package Shopware\Plugins\SwagMigration\Components
+ * @copyright Copyright (c) 2012, shopware AG (http://www.shopware.de)
  */
 class Shopware_Components_Migration_Profile_Prestashop extends Shopware_Components_Migration_Profile
 {
