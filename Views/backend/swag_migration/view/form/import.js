30076ec10c147ed26f0f4cbc0bb36eba98a21b05
Dennis Garding <di@shopware.com>
2017-04-07 07:40:06 +0200

    PT-7552 - remove show on woo

--- Views/backend/swag_migration/view/form/import.js
+++ Views/backend/swag_migration/view/form/import.js
@@ -43,11 +43,6 @@ Ext.define('Shopware.apps.SwagMigration.view.form.Import', {
     saltInputNeeded: false,
 
     /**
-     * will be set to true if the current profile needs an additional salt to be entered by the user
-     */
-    showOnWoo: false,
-
-    /**
      * Will be set to true, if the migration plugin provides password encoder for the selected profile
      */
     showPasswordInfo: false,
@@ -158,7 +153,7 @@ Ext.define('Shopware.apps.SwagMigration.view.form.Import', {
 
     getLeftItems: function() {
         var me = this,
-            items = [];
+            items;
 
         items = [{
             fieldLabel: '{s name=importProducts}Import products{/s}',
@@ -168,14 +163,10 @@ Ext.define('Shopware.apps.SwagMigration.view.form.Import', {
             fieldLabel: '{s name=importTranslations}Import translations{/s}',
             name: 'import_translations',
             xtype: 'checkbox',
-            hidden: !me.showOnWoo,
-            checked: me.showOnWoo
         }, {
             fieldLabel: '{s name=importProperties}Import product properties{/s}',
             name: 'import_properties',
             xtype: 'checkbox',
-            hidden: !me.showOnWoo,
-            checked: me.showOnWoo
         }, {
             fieldLabel: '{s name=importCategories}Import categories{/s}',
             name: 'import_categories',
@@ -190,8 +181,6 @@ Ext.define('Shopware.apps.SwagMigration.view.form.Import', {
             fieldLabel: '{s name=importPrices}Import customer group prices{/s}',
             name: 'import_prices',
             xtype: 'checkbox',
-            hidden: !me.showOnWoo,
-            checked: me.showOnWoo
         }, {
             fieldLabel: '{s name=generateVariants}Generate variants from attributes{/s}',
             name: 'import_generate_variants',
@@ -443,12 +432,6 @@ Ext.define('Shopware.apps.SwagMigration.view.form.Import', {
             me.saltInput.hide();
             me.saltInput.allowBlank = true;
         }
-    },
-
-    setWooCommerceSettings: function(value) {
-        var me = this;
-
-        me.showOnWoo = value;
     },
 
     setImportAllowed: function(value) {
