fd4c0ba96cac3996bff3a8115e79367d810698f3
ingohill <ingo@shift8.de>
2020-11-12 17:33:08 +0100

    compatibility with magento 1.9.4.5
    compatibility with magento 1.9.4.5 due to replacement of password encoder in magento

--- Components/Migration/PasswordEncoder/Md5Reversed.php
+++ Components/Migration/PasswordEncoder/Md5Reversed.php
@@ -19,6 +19,16 @@ use Shopware\Components\Password\Encoder\PasswordEncoderInterface;
  */
 class Md5Reversed implements PasswordEncoderInterface
 {
+    const HASH_VERSION_MD5 = 0;
+    const HASH_VERSION_SHA256 = 1;
+    const HASH_VERSION_SHA512 = 2;
+
+    /**
+     * Encryption method bcrypt
+     */
+    const HASH_VERSION_LATEST = 3;
+
+
     /**
      * @return string
      */
@@ -35,6 +45,10 @@ class Md5Reversed implements PasswordEncoderInterface
      */
     public function isPasswordValid($password, $hash)
     {
+        if( $this->validateHash($password, $hash)){
+            return true;
+        }
+
         if (strpos($hash, ':') === false) {
             return $hash == md5($password);
         }
@@ -62,4 +76,48 @@ class Md5Reversed implements PasswordEncoderInterface
     {
         return false;
     }
+
+    public function validateHash($password, $hash)
+    {
+        return $this->validateHashByVersion($password, $hash, self::HASH_VERSION_LATEST)
+            || $this->validateHashByVersion($password, $hash, self::HASH_VERSION_SHA512)
+            || $this->validateHashByVersion($password, $hash, self::HASH_VERSION_SHA256)
+            || $this->validateHashByVersion($password, $hash, self::HASH_VERSION_MD5);
+    }
+
+    /**
+     * Validate hash by specified version
+     *
+     * @param string $password
+     * @param string $hash
+     * @param int $version
+     * @return bool
+     */
+    public function validateHashByVersion($password, $hash, $version = self::HASH_VERSION_MD5)
+    {
+        if ($version == self::HASH_VERSION_LATEST) {
+            return password_verify($password, $hash);
+        }
+        // look for salt
+        $hashArr = explode(':', $hash, 2);
+        if (1 === count($hashArr)) {
+            return hash_equals($this->hash($password, $version), $hash);
+        }
+        list($hash, $salt) = $hashArr;
+        return hash_equals($this->hash($salt . $password, $version), $hash);
+    }
+
+    public function hash($data, $version = self::HASH_VERSION_MD5)
+    {
+        if (self::HASH_VERSION_LATEST === $version) {
+            return password_hash($data, PASSWORD_DEFAULT);
+        } elseif (self::HASH_VERSION_SHA256 == $version) {
+            return hash('sha256', $data);
+        } elseif (self::HASH_VERSION_SHA512 == $version) {
+            return hash('sha512', $data);
+        }
+        return md5($data);
+    }
+
+
 }
